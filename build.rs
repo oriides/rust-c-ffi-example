037f232ae863608f8acd657a05bde11d5856dff8
Niko Thees <nikolaithees@gmail.com>
2022-05-15 12:51:53 +0000

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
     // Tell Cargo that if the given file changes, to rerun this build script.
-    // println!("cargo:rerun-if-changed=src/include/cstruct.h");
+    println!("cargo:rerun-if-changed=src/include/cstruct.h");
     println!("cargo:rustc-link-search=.");
     println!("cargo:rustc-link-lib=static=cstruct");
     // Use the `cc` crate to build a C file and statically link it.
